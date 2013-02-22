23ed4119c4d7bbd8dadcef69d6d0c79e57ad4c43
Ryan Fairchild <rfairchild@glgroup.com>
2013-02-22 16:22:59 -0500

    Making iteration safe

--- lib/xapian_fu/xapian_array_match_spy.rb
+++ lib/xapian_fu/xapian_array_match_spy.rb
@@ -3,6 +3,7 @@ class XapianFu::ArrayCountMatchSpy < Xapian::ValueCountMatchSpy
 		values = []	
 	  	Xapian._safelyIterate(self._dangerous_values_begin(), self._dangerous_values_end()) do |item|
 		  	terms = YAML::load(item.term)
+		  	terms ||= []
 		  	terms.each do |term|
 		  		idx = values.find_index { |val| val.term == term }				
 				values[idx].termfreq += 1 if idx 
@@ -16,6 +17,7 @@ class XapianFu::ArrayCountMatchSpy < Xapian::ValueCountMatchSpy
 		values = []
 	  	Xapian._safelyIterate(self._dangerous_top_values_begin(maxvalues), self._dangerous_top_values_end(maxvalues)) do |item|		  	
 		  	terms = YAML::load(item.term)
+		  	terms ||= []
 		  	terms.each do |term|
 		  		idx = values.find_index { |val| val.term == term }				
 				values[idx].termfreq += 1 if idx 
